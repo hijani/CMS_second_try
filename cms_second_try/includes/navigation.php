57bde28768dfae4c1d6933bce0f288bb4702c8a1
hijani <hijan29714@gmail.com>
2022-09-25 17:11:10 -0400

    dfsgdf

--- cms_second_try/includes/navigation.php
+++ cms_second_try/includes/navigation.php
@@ -8,7 +8,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Start Bootstrap</a>
+                <a class="navbar-brand" href="#">CMS</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -18,11 +18,17 @@
                     
                         $query = "SELECT * FROM categories";
                         $categories_connection = mysqli_query($connection, $query);
+
+                        while($row = mysqli_fetch_assoc($categories_connection)) {
+                            $cat_title = $row['cat_title'];
+                            $cat_id = $row['cat_id'];
+
+                            echo "<li><a href='#'>$cat_title</a></li>";
+                            
+                        }
                     
                     ?>
-                    <li>
-                        <a href="#">About</a>
-                    </li>
+                    
                 </ul>
             </div>
             <!-- /.navbar-collapse -->

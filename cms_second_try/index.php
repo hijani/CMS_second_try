99378480cb125d2dab25c917e35e0dfb9926ce51
hijani <hijan29714@gmail.com>
2022-09-25 16:58:02 -0400

    dfsd

--- cms_second_try/index.php
+++ cms_second_try/index.php
@@ -17,41 +17,10 @@
             <div class="col-md-4">
 
                 <!-- Blog Search Well -->
-                
+                <?php include "includes/blog_search.php"; ?>
 
                 <!-- Blog Categories Well -->
-                <div class="well">
-                    <h4>Blog Categories</h4>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.col-lg-6 -->
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.col-lg-6 -->
-                    </div>
-                    <!-- /.row -->
-                </div>
+                
 
                 <!-- Side Widget Well -->
                 <div class="well">

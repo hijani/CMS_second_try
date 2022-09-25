ec56cd6d1df791abaf55b50f5c1459b4250ad028
hijani <hijan29714@gmail.com>
2022-09-25 17:41:35 -0400

    dsfsd

--- cms_second_try/includes/blog_search.php
+++ cms_second_try/includes/blog_search.php
@@ -1,9 +1,9 @@
 <div class="well">
     <h4>Blog Search</h4>
     <div class="input-group">
-        <input name="post_search" type="text" class="form-control">
+        <input name="post_search_text" type="text" class="form-control">
         <span class="input-group-btn">
-            <button class="btn btn-default" type="button">
+            <button name="post_search" class="btn btn-default" type="button">
                 <span class="glyphicon glyphicon-search"></span>
             </button>
         </span>

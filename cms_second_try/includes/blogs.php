e4468f4d2d96d65867cf09a3f924659d74af697d
hijani <hijan29714@gmail.com>
2022-09-25 17:35:48 -0400

    dsfdsf

--- cms_second_try/includes/blogs.php
+++ cms_second_try/includes/blogs.php
@@ -5,21 +5,36 @@
         <small>Secondary Text</small>
     </h1>
 
-    <!-- First Blog Post -->
-    <h2>
-        <a href="#">Blog Post Title</a>
-    </h2>
-    <p class="lead">
-        by <a href="index.php">Start Bootstrap</a>
-    </p>
-    <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-    <hr>
-    <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-    <hr>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-    <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+    <?php 
 
-    <hr>
+        $query = "SELECT * FROM posts";
+        $post_query = mysqli_query($connection, $query);
+
+        while($row = mysqli_fetch_assoc($post_query)) {
+            $post_id = $row['post_id'];
+            $post_category_id = $row['post_category_id'];
+            $post_title = $row['post_title'];
+            $post_author = $row['post_author'];
+            $post_date = $row['post_date'];
+            $post_image = $row['post_image'];
+            $post_content = $row['post_content'];
+
+    ?>
+            <h2>
+                <a href="#"><?php echo $post_title; ?></a>
+            </h2>
+            <p class="lead">
+                by <a href="index.php"><?php echo $post_author; ?></a>
+            </p>
+            <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
+            <hr>
+            <img class="img-responsive" src="<?php echo $post_image; ?>" alt="some images">
+            <hr>
+            <p><?php echo $post_content; ?></p>
+            <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+            <hr>
+    <?php } ?>
 
     <!-- Pager -->
     <ul class="pager">

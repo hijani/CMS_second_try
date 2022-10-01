7207ab2132a241c82d520ac3f4327ddea9dcaa18
hijani <hijan29714@gmail.com>
2022-10-01 19:36:04 -0400

    dfd

--- cms_second_try/admin/includes/sidebar_navigation.php
+++ cms_second_try/admin/includes/sidebar_navigation.php
@@ -7,22 +7,13 @@
             <a href="categories.php"><i class="fa fa-list fa-list-alt"></i> Categories</a>
         </li>
         <li>
-            <a href="forms.html"><i class="fa fa-fw fa-edit"></i> Forms</a>
-        </li>
-        <li>
-            <a href="bootstrap-elements.html"><i class="fa fa-fw fa-desktop"></i> Bootstrap Elements</a>
-        </li>
-        <li>
-            <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Bootstrap Grid</a>
-        </li>
-        <li>
-            <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Dropdown <i class="fa fa-fw fa-caret-down"></i></a>
+            <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
             <ul id="demo" class="collapse">
                 <li>
-                    <a href="#">Dropdown Item</a>
+                    <a href="view_all_posts.php">View All Posts</a>
                 </li>
                 <li>
-                    <a href="#">Dropdown Item</a>
+                    <a href="#">Add posts</a>
                 </li>
             </ul>
         </li>

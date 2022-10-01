57c698ad401d6d36033afc15da64a7bc1ae0b0d1
hijani <hijan29714@gmail.com>
2022-10-01 16:31:20 -0400

    sdfdsf

--- cms_second_try/includes/blog_search.php
+++ cms_second_try/includes/blog_search.php
@@ -1,14 +1,8 @@
 <div class="well">
     <h4>Blog Search</h4>
 
-    <?php 
-        if(isset($_POST['post_search'])) {
-            $search_text = $_POST['post_search_text'];
-            
-        }
     
-    ?>
-    <form action="" method="post">
+    <form action="search.php" method="post">
         <div class="input-group">
             <input name="post_search_text" type="text" class="form-control">
             <span class="input-group-btn">
--- cms_second_try/search.php
+++ cms_second_try/search.php
@@ -21,35 +21,57 @@
             </h1>
 
             <?php 
+                if(isset($_POST['post_search'])) {
+                    $search_text = $_POST['post_search_text'];
 
-                $query = "SELECT * FROM posts";
-                $post_query = mysqli_query($connection, $query);
+                    $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search_text%' ";
+                    $search_query = mysqli_query($connection, $query);
 
-                while($row = mysqli_fetch_assoc($post_query)) {
-                    $post_id = $row['post_id'];
-                    $post_category_id = $row['post_category_id'];
-                    $post_title = $row['post_title'];
-                    $post_author = $row['post_author'];
-                    $post_date = $row['post_date'];
-                    $post_image = $row['post_image'];
-                    $post_content = $row['post_content'];
+                    if(!$search_query) {
+                        die('Query Failed' . mysqli_error($connection));
+                    }
 
+                    $count_rows = mysqli_num_rows($search_query);
+
+                    if($count_rows == 0) {
+                        echo "<h1>No Result</h1>";
+                    } else {
+
+                        $query = "SELECT * FROM posts";
+                        $post_query = mysqli_query($connection, $query);
+
+                        while($row = mysqli_fetch_assoc($post_query)) {
+                            $post_id = $row['post_id'];
+                            $post_category_id = $row['post_category_id'];
+                            $post_title = $row['post_title'];
+                            $post_author = $row['post_author'];
+                            $post_date = $row['post_date'];
+                            $post_image = $row['post_image'];
+                            $post_content = $row['post_content'];
+
+                    ?>
+                            <h2>
+                                <a href="#"><?php echo $post_title; ?></a>
+                            </h2>
+                            <p class="lead">
+                                by <a href="index.php"><?php echo $post_author; ?></a>
+                            </p>
+                            <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
+                            <hr>
+                            <img class="img-responsive" src="<?php echo $post_image; ?>" alt="some images">
+                            <hr>
+                            <p><?php echo $post_content; ?></p>
+                            <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                            <hr>
+                    <?php }
+                    }
+                    
+                }
+    
             ?>
-                    <h2>
-                        <a href="#"><?php echo $post_title; ?></a>
-                    </h2>
-                    <p class="lead">
-                        by <a href="index.php"><?php echo $post_author; ?></a>
-                    </p>
-                    <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
-                    <hr>
-                    <img class="img-responsive" src="<?php echo $post_image; ?>" alt="some images">
-                    <hr>
-                    <p><?php echo $post_content; ?></p>
-                    <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
-                    <hr>
-            <?php } ?>
+            
 
             <!-- Pager -->
             <ul class="pager">

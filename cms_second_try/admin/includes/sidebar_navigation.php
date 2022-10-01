6d889be9f8773ed06f7499672d4838afef61205d
hijani <hijan29714@gmail.com>
2022-10-01 16:51:14 -0400

    dsafsdf

--- cms_second_try/admin/includes/sidebar_navigation.php
+++ cms_second_try/admin/includes/sidebar_navigation.php
@@ -4,10 +4,7 @@
             <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
         </li>
         <li>
-            <a href="charts.html"><i class="fa fa-fw fa-bar-chart-o"></i> Charts</a>
-        </li>
-        <li>
-            <a href="tables.html"><i class="fa fa-fw fa-table"></i> Tables</a>
+            <a href="categories.php"><i class="fa fa-fw fa-table"></i> Categories</a>
         </li>
         <li>
             <a href="forms.html"><i class="fa fa-fw fa-edit"></i> Forms</a>

cfae1e63a42daa91854396614204a87b2f3ca1f0
hijani <hijan29714@gmail.com>
2022-10-01 19:32:42 -0400

    sdfsd

--- cms_second_try/admin/view_all_posts.php
+++ cms_second_try/admin/view_all_posts.php
@@ -17,17 +17,8 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            Blank Page
-                            <small>Subheading</small>
+                            Posts
                         </h1>
-                        <ol class="breadcrumb">
-                            <li>
-                                <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                            </li>
-                            <li class="active">
-                                <i class="fa fa-file"></i> Blank Page
-                            </li>
-                        </ol>
                     </div>
                 </div>
                 <!-- /.row -->

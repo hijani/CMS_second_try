b0dacdb9fd73cfed3d57db5996779b86e6801171
hijani <hijan29714@gmail.com>
2022-10-01 19:46:53 -0400

    dsfd

--- cms_second_try/admin/view_all_posts.php
+++ cms_second_try/admin/view_all_posts.php
@@ -25,17 +25,54 @@
                     <div class="col-md-12">
                         <table class="table">
                             <tr>
-                                <th></th>
-                                <th></th>
-                                <th></th>
-                                <th></th>
-                                <th></th>
-                                <th></th>
-                                <th></th>
-                                <th></th>
-                                <th></th>
-                                <th></th>
+                                <th>Post ID</th>
+                                <th>Post Title</th>
+                                <th>Post Author</th>
+                                <th>Post Date</th>
+                                <th>Post Image</th>
+                                <th>Post Content</th>
+                                <th>Post Tags</th>
+                                <th>Post Comment Count</th>
+                                <th>Post Status</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
                             </tr>
+                            <?php 
+                                $query = "SELECT * FROM posts";
+                                $post_query = mysqli_query($connection, $query);
+
+                                if(!$post_query) {
+                                    die("query failed" . mysqli_error($connection));
+                                }
+
+                                while ($row = mysqli_fetch_assoc($post_query)) {
+
+                                    $post_id = $row['post_id'];
+                                    $post_title = $row['post_title'];
+                                    $post_author = $row['post_author'];
+                                    $post_date = $row['post_date'];
+                                    $post_image = $row['post_image'];
+                                    $post_content = $row['post_content'];
+                                    $post_tags = $row['post_tags'];
+                                    $post_comment_count = $row['post_comment_count'];
+                                    $post_status = $row['post_status'];
+
+
+                                    echo '<tr>';
+                                    echo "<td>$post_id</td>";
+                                    echo "<td>$post_title</td>";
+                                    echo "<td>$post_author</td>";
+                                    echo "<td>$post_date</td>";
+                                    echo "<td>$post_image</td>";
+                                    echo "<td>$post_content</td>";
+                                    echo "<td>$post_tags</td>";
+                                    echo "<td>$post_comment_count</td>";
+                                    echo "<td>$post_status</td>";
+                                    echo "<td><a href=''>Edit</a></td>";
+                                    echo "<td><a href=''>Delete</a></td>";
+                                    echo '</tr>';
+                                }
+                            ?>
                         </table>
                     </div>
                 </div>

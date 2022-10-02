26e8ce0b8014707d5e8ea8a99e520df10d98bfda
hijani <hijan29714@gmail.com>
2022-10-01 21:04:31 -0400

    fgdfg

--- cms_second_try/admin/includes/add_post.php
+++ cms_second_try/admin/includes/add_post.php
@@ -3,34 +3,19 @@
         <input type="text" name="post_title" class="form-control" placeholder="Post Title">
     </div>
     <div class="form-group">
-        <input type="text" name="post_title" class="form-control" placeholder="Post Title">
+        <input type="text" name="post_author" class="form-control" placeholder="Post Author">
     </div>
     <div class="form-group">
-        <input type="text" name="post_title" class="form-control" placeholder="Post Title">
+        <input type="file" name="post_image" class="form-control">
     </div>
     <div class="form-group">
-        <input type="text" name="post_title" class="form-control" placeholder="Post Title">
+        <textarea name="post_content" cols="30" rows="10" placeholder="Post Content"></textarea>
     </div>
     <div class="form-group">
-        <input type="text" name="post_title" class="form-control" placeholder="Post Title">
+        <input type="text" name="post_tags" class="form-control" placeholder="Post Title">
     </div>
     <div class="form-group">
-        <input type="text" name="post_title" class="form-control" placeholder="Post Title">
-    </div>
-    <div class="form-group">
-        <input type="text" name="post_title" class="form-control" placeholder="Post Title">
-    </div>
-    <div class="form-group">
-        <input type="text" name="post_title" class="form-control" placeholder="Post Title">
-    </div>
-    <div class="form-group">
-        <input type="text" name="post_title" class="form-control" placeholder="Post Title">
-    </div>
-    <div class="form-group">
-        <input type="text" name="post_title" class="form-control" placeholder="Post Title">
-    </div>
-    <div class="form-group">
-        <input type="text" name="post_title" class="form-control" placeholder="Post Title">
+        <input type="text" name="post_status" class="form-control" placeholder="Post Title">
     </div>
     
 </form>

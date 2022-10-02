4a8fab228a823d598668f36fcf8b5d1ee1dce2ee
hijani <hijan29714@gmail.com>
2022-10-01 22:15:29 -0400

    dsfsdf

--- cms_second_try/admin/includes/update_post.php
+++ cms_second_try/admin/includes/update_post.php
@@ -20,16 +20,19 @@
         $query.= " VALUES ('$post_title', $post_category_id, ";
         $query.= "'$post_author', now(), '$post_image', ";
         $query.= "'$post_content', '$post_tags', $post_comment_count, '$post_status') ";
-        $query .= "WHERE post_id = $post_id ";
+        $query .= " WHERE post_id = $post_id ";
         $update_post_query = mysqli_query($connection, $query);
 
         move_uploaded_file($post_image_temp, "../images/$post_image");
+    }   
 
-        while ($row = mysqli_fetch_assoc($update_post_query)) {
+        $query = "SELECT * FROM posts WHERE post_id = $post_id";
+        $show_update_post = mysqli_query($connection, $query);
+
+        while ($row = mysqli_fetch_assoc($show_update_post)) {
             $post_title = $row['post_title'];
             $post_category_id = $row['post_category_id'];
             $post_author = $row['post_author'];
-            $post_image = $row['post_image'];
             $post_content = $row['post_content'];
             $post_tags = $row['post_tags'];
             $post_status = $row['post_status'];
@@ -37,37 +40,31 @@
 ?>
             <form action="" method="post" enctype="multipart/form-data">
                 <div class="form-group">
-                    <input type="text" name="post_title" class="form-control" placeholder="Post Title">
+                    <input value="<?php echo $post_title; ?>" type="text" name="post_title" class="form-control" placeholder="Post Title">
                 </div>
                 <div class="form-group">
-                    <input type="text" name="post_category_id" class="form-control" placeholder="Post Category ID">
+                    <input type="text" value="<?php echo $post_category_id; ?>" name="post_category_id" class="form-control" placeholder="Post Category ID">
                 </div>
                 <div class="form-group">
-                    <input type="text" name="post_author" class="form-control" placeholder="Post Author">
+                    <input value="<?php echo $post_author; ?>" type="text" name="post_author" class="form-control" placeholder="Post Author">
                 </div>
                 <div class="form-group">
                     <input type="file" name="post_image" class="form-control">
                 </div>
                 <div class="form-group">
-                    <textarea name="post_content" cols="30" rows="10" placeholder="Post Content"></textarea>
+                    <textarea name="post_content" cols="30" rows="10" placeholder="Post Content"><?php echo $post_content; ?></textarea>
                 </div>
                 <div class="form-group">
-                    <input type="text" name="post_tags" class="form-control" placeholder="Post Tags">
+                    <input value="<?php echo $post_tags; ?>" type="text" name="post_tags" class="form-control" placeholder="Post Tags">
                 </div>
                 <div class="form-group">
-                    <input type="text" name="post_status" class="form-control" placeholder="Post Status">
+                    <input value="<?php echo $post_status; ?>" type="text" name="post_status" class="form-control" placeholder="Post Status">
                 </div>
                 <input type="submit" value="Update Post" name="update_post" class="btn btn-primary">
                 
             </form>
 
 
-<?php
-        }
-        
-
-    }
-
-?>
+<?php   } ?>
 
 

19706870814ceaecee0e16500af482f8bdb8a228
hijani <hijan29714@gmail.com>
2022-10-01 21:40:26 -0400

    dsfds

--- cms_second_try/admin/includes/add_post.php
+++ cms_second_try/admin/includes/add_post.php
@@ -1,25 +1,25 @@
 <?php 
 
     if(isset($_POST['add_post'])) {
-        // $post_title = $_POST['post_title'];
-        // $post_category_id = $_POST['post_category_id'];
-        // $post_author = $_POST['post_author'];
+        $post_title = $_POST['post_title'];
+        $post_category_id = $_POST['post_category_id'];
+        $post_author = $_POST['post_author'];
         $post_image = $_FILES['post_image']['name'];
         $post_image_temp = $_FILES['post_image']['tmp_name'];
-        // $post_content = $_POST['post_content'];
-        // $post_tags = $_POST['post_tags'];
-        // $post_status = $_POST['post_status'];
-        // $post_comment_count = 4;
+        $post_content = $_POST['post_content'];
+        $post_tags = $_POST['post_tags'];
+        $post_status = $_POST['post_status'];
+        $post_comment_count = 4;
 
-        // $query = "INSERT INTO posts (post_title, ";
-        // $query .= "post_category_id, post_author, ";
-        // $query .= "post_date, post_image, post_content, ";
-        // $query.= "post_tags, post_comment_count, post_status) ";
+        $query = "INSERT INTO posts (post_title, ";
+        $query .= "post_category_id, post_author, ";
+        $query .= "post_date, post_image, post_content, ";
+        $query.= "post_tags, post_comment_count, post_status) ";
 
-        // $query.= " VALUES ('$post_title', $post_category_id, ";
-        // $query.= "'$post_author', now(), '$post_image', ";
-        // $query.= "'$post_content', '$post_tags', $post_comment_count, '$post_status') ";
-        // $add_post_query = mysqli_query($connection, $query);
+        $query.= " VALUES ('$post_title', $post_category_id, ";
+        $query.= "'$post_author', now(), '$post_image', ";
+        $query.= "'$post_content', '$post_tags', $post_comment_count, '$post_status') ";
+        $add_post_query = mysqli_query($connection, $query);
 
         move_uploaded_file($post_image_temp, "../images/$post_image");
         echo $post_image_temp;
@@ -32,7 +32,7 @@
 
 
 <form action="" method="post">
-    <!-- <div class="form-group">
+    <div class="form-group">
         <input type="text" name="post_title" class="form-control" placeholder="Post Title">
     </div>
     <div class="form-group">
@@ -40,11 +40,11 @@
     </div>
     <div class="form-group">
         <input type="text" name="post_author" class="form-control" placeholder="Post Author">
-    </div> -->
+    </div>
     <div class="form-group">
         <input type="file" name="post_image" class="form-control">
     </div>
-    <!-- <div class="form-group">
+    <div class="form-group">
         <textarea name="post_content" cols="30" rows="10" placeholder="Post Content"></textarea>
     </div>
     <div class="form-group">
@@ -52,7 +52,7 @@
     </div>
     <div class="form-group">
         <input type="text" name="post_status" class="form-control" placeholder="Post Status">
-    </div> -->
+    </div>
     <input type="submit" value="Add Post" name="add_post" class="btn btn-primary">
     
 </form>

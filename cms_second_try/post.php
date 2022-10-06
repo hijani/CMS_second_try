16b3afce75ff1004a3a418f44c543716e75af2d3
hijani <hijan29714@gmail.com>
2022-10-05 22:44:32 -0400

    dsfds

--- cms_second_try/post.php
+++ cms_second_try/post.php
@@ -31,7 +31,6 @@
                 }
 
                 $query = "SELECT * FROM posts WHERE post_id = $post_id ";
-                // $query .= " OR post_category_id = $category_id";
                 $post_query = mysqli_query($connection, $query);
 
                 while($row = mysqli_fetch_assoc($post_query)) {

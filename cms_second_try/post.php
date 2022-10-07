390a2b321fc2713c5ab512c90b295652c95c80b1
hijani <hijan29714@gmail.com>
2022-10-06 21:36:44 -0400

    sdfsd

--- cms_second_try/post.php
+++ cms_second_try/post.php
@@ -66,6 +66,7 @@
                     <h4>Leave a Comment:</h4>
                     <form role="form" method="post" action="">
                         <div class="form-group">
+                            <label for="comment_content">Your Message</label>
                             <textarea name="comment_content" class="form-control" rows="3"></textarea>
                         </div>
                         <button type="add_comment" class="btn btn-primary">Submit</button>

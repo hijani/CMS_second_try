6960d8e6252784b55ec158b4881937372b86cee1
hijani <hijan29714@gmail.com>
2022-10-06 21:22:25 -0400

    dsaf

--- cms_second_try/post.php
+++ cms_second_try/post.php
@@ -79,7 +79,7 @@
                 <!-- Comment -->
                 <div class="media">
                     <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
+                        <img class="media-object" src="http://placehold.it/64x64" alt="person-pic">
                     </a>
                     <div class="media-body">
                         <h4 class="media-heading">Start Bootstrap

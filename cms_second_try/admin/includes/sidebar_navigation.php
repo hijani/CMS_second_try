e0f8195ef94c52f8c8faf9ccd910cafefec679b6
hijani <hijan29714@gmail.com>
2022-10-06 22:09:33 -0400

    sdfsd

--- cms_second_try/admin/includes/sidebar_navigation.php
+++ cms_second_try/admin/includes/sidebar_navigation.php
@@ -7,18 +7,18 @@
             <a href="categories.php"><i class="fa fa-list fa-list-alt"></i> Categories</a>
         </li>
         <li>
-            <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
-            <ul id="demo" class="collapse">
+            <a href="javascript:;" data-toggle="collapse" data-target="#post"><i class="fa fa-fw fa-arrows-v"></i> Posts<i class="fa fa-fw fa-caret-down"></i></a>
+            <ul id="post" class="collapse">
                 <li>
-                    <a href="posts.php">View All Posts</a>
+                    <a href="posts.php"> View All Posts</a>
                 </li>
                 <li>
-                    <a href="posts.php?source=add_post">Add posts</a>
+                    <a href="posts.php?source=add_post"> Add posts</a>
                 </li>
             </ul>
         </li>
         <li class="active">
-            <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Blank Page</a>
+            <a href="comments.php"><i class="fa fa-fw fa-comment"></i> Comments</a>
         </li>
         <li>
             <a href="index-rtl.html"><i class="fa fa-fw fa-dashboard"></i> RTL Dashboard</a>

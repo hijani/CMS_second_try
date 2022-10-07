960aa6289f7c9531b818e92472eaa0e248f65635
hijani <hijan29714@gmail.com>
2022-10-06 22:23:05 -0400

    dsfdf

--- cms_second_try/admin/includes/view_all_comments.php
+++ cms_second_try/admin/includes/view_all_comments.php
@@ -17,29 +17,20 @@
         }
 
         while ($row = mysqli_fetch_assoc($comment_query)) {
-            
-
-            
+            $comment_id = $row['comment_id'];
+            $comment_post_id = $row['comment_post_id'];
+            $comment_author = $row['comment_author'];
+            $comment_email = $row['comment_email'];
+            $comment_content = $row['comment_content'];
+            $comment_date = $row['comment_date'];
+            $comment_status = $row['comment_status'];
 
 
             echo '<tr>';
-            echo "<td>$Comment_title</td>";
-            
-            $query = "SELECT * FROM categories WHERE cat_id = $post_category";
-            $edit_category_query = mysqli_query($connection, $query);
-
-            while($row = mysqli_fetch_assoc($edit_category_query)) {
-                $cat_title = $row['cat_title'];
-                $cat_id = $row['cat_id'];
-            }
-
-            echo "<td><a href='../categories.php?category_id=$cat_id'>$cat_title</a></td>";
             echo "<td>$Comment_author</td>";
-            echo "<td>$Comment_date</td>";
-            echo "<td><img src='../images/$Comment_image' width='100px'></td>";
+            echo "<td>$Comment_email</td>";
             echo "<td>$Comment_content</td>";
-            echo "<td>$Comment_tags</td>";
-            echo "<td>$Comment_comment_count</td>";
+            echo "<td>$Comment_date</td>";
             echo "<td>$Comment_status</td>";
             echo "<td><a href='Comments.php?source=update_Comment&update_Comment_id=$Comment_id'>Edit</a></td>";
             echo "<td><a href='Comments.php?delete=$Comment_id'>Delete</a></td>";

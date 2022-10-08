e8846c615cb36f0e028c6a93a5298f1661486011
hijani <hijan29714@gmail.com>
2022-10-08 11:21:06 -0400

    dsf

--- cms_second_try/admin/includes/view_all_comments.php
+++ cms_second_try/admin/includes/view_all_comments.php
@@ -54,9 +54,9 @@
             $query = "DELETE FROM comments WHERE comment_id='$comment_id'";
             $delete_query = mysqli_query($connection, $query);
             header("Location: comments.php");
-            
+
             $query = "UPDATE posts SET post_comment_count = post_comment_count - 1 ";
-            $query .= "WHERE post_id = $post_id";
+            $query .= "WHERE post_id = $comment_post_id";
             $decrease_comment_count = mysqli_query($connection, $query);
         }
 

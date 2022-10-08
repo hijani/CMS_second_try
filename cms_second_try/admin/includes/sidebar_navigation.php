83280c9f03473b8e9862654eb169a4005cd4e9c2
hijani <hijan29714@gmail.com>
2022-10-08 15:52:12 -0400

    cxvcv

--- cms_second_try/admin/includes/sidebar_navigation.php
+++ cms_second_try/admin/includes/sidebar_navigation.php
@@ -1,24 +1,24 @@
 <div class="collapse navbar-collapse navbar-ex1-collapse">
     <ul class="nav navbar-nav side-nav">
-        <li>
-            <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+        <li class="nav-item">
+            <a href="index.php" class="nav-link"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
         </li>
-        <li>
-            <a href="categories.php"><i class="fa fa-list fa-list-alt"></i> Categories</a>
+        <li class="nav-item">
+            <a href="categories.php" class="nav-link"><i class="fa fa-list fa-list-alt"></i> Categories</a>
         </li>
-        <li>
+        <li class="nav-item">
             <a href="javascript:;" data-toggle="collapse" data-target="#post"><i class="fa fa-fw fa-arrows-v"></i> Posts<i class="fa fa-fw fa-caret-down"></i></a>
             <ul id="post" class="collapse">
-                <li>
-                    <a href="posts.php"> View All Posts</a>
+                <li class="nav-item">
+                    <a href="posts.php" class="nav-link"> View All Posts</a>
                 </li>
-                <li>
-                    <a href="posts.php?source=add_post"> Add posts</a>
+                <li class="nav-item">
+                    <a href="posts.php?source=add_post" class="nav-link"> Add posts</a>
                 </li>
             </ul>
         </li>
-        <li>
-            <a href="comments.php"><i class="fa fa-fw fa-comment"></i> Comments</a>
+        <li class="nav-item">
+            <a href="comments.php" class="nav-link"><i class="fa fa-fw fa-comment"></i> Comments</a>
         </li>
     </ul>
 </div>

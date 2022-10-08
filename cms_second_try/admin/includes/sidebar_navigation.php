8b585ae5e2b5ac905ae089063eac6c0e95cac9f7
hijani <hijan29714@gmail.com>
2022-10-08 11:35:20 -0400

    sdf

--- cms_second_try/admin/includes/sidebar_navigation.php
+++ cms_second_try/admin/includes/sidebar_navigation.php
@@ -1,23 +1,23 @@
 <div class="collapse navbar-collapse navbar-ex1-collapse">
     <ul class="nav navbar-nav side-nav">
-        <li>
+        <li class="sidebar-nav-link">
             <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
         </li>
-        <li>
+        <li class="sidebar-nav-link">
             <a href="categories.php"><i class="fa fa-list fa-list-alt"></i> Categories</a>
         </li>
         <li>
             <a href="javascript:;" data-toggle="collapse" data-target="#post"><i class="fa fa-fw fa-arrows-v"></i> Posts<i class="fa fa-fw fa-caret-down"></i></a>
             <ul id="post" class="collapse">
-                <li>
+                <li class="sidebar-nav-link">
                     <a href="posts.php"> View All Posts</a>
                 </li>
-                <li>
+                <li class="sidebar-nav-link">
                     <a href="posts.php?source=add_post"> Add posts</a>
                 </li>
             </ul>
         </li>
-        <li>
+        <li class="sidebar-nav-link">
             <a href="comments.php"><i class="fa fa-fw fa-comment"></i> Comments</a>
         </li>
     </ul>

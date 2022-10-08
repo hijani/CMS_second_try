f2c8f5dbd8dddecd047f3aa9da0d21e322f686bc
hijani <hijan29714@gmail.com>
2022-10-08 11:22:50 -0400

    dsfd

--- cms_second_try/search.php
+++ cms_second_try/search.php
@@ -51,7 +51,7 @@
 
                     ?>
                             <h2>
-                                <a href="#"><?php echo $post_title; ?></a>
+                                <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
                             </h2>
                             <p class="lead">
                                 by <a href="index.php"><?php echo $post_author; ?></a>
